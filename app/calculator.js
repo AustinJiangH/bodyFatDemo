061ce2f01946c50b65d79ee84d3a4c0ae27804ee
Austin <ab12ab12@outlook.com>
2020-10-23 18:37:02 -0500

    update model v2

--- app/calculator.js
+++ app/calculator.js
@@ -18,13 +18,13 @@ var app = new Vue({
         hip: "",
 
         // advanced 
-        adiposity: "",
+        // adiposity: "",
         neck: "",
         abdomen: "",
-        thigh: "",
-        knee: "",
-        ankle: "",
-        biceps: "",
+        // thigh: "",
+        // knee: "",
+        // ankle: "",
+        // biceps: "",
         forearm: "",
 
         bodyfat: "",
@@ -46,9 +46,13 @@ var app = new Vue({
 
 
         check: function (category) {
+            // a = (this.gender == "m" || this.gender == "f") && this.validNum(this.age) && this.validNum(this.height) && this.validNum(this.weight);
+            // b = this.validNum(this.chest) && this.validNum(this.wrist) && this.validNum(this.hip);
+            // c = this.validNum(this.adiposity) && this.validNum(this.neck) && this.validNum(this.abdomen) && this.validNum(this.thigh) && this.validNum(this.knee) && this.validNum(this.ankle) && this.validNum(this.biceps) && this.validNum(this.forearm);
+
             a = (this.gender == "m" || this.gender == "f") && this.validNum(this.age) && this.validNum(this.height) && this.validNum(this.weight);
             b = this.validNum(this.chest) && this.validNum(this.wrist) && this.validNum(this.hip);
-            c = this.validNum(this.adiposity) && this.validNum(this.neck) && this.validNum(this.abdomen) && this.validNum(this.thigh) && this.validNum(this.knee) && this.validNum(this.ankle) && this.validNum(this.biceps) && this.validNum(this.forearm);
+            c = this.validNum(this.neck) && this.validNum(this.abdomen) && this.validNum(this.forearm);
 
             switch (category) {
                 case 1:
@@ -78,7 +82,7 @@ var app = new Vue({
                             break;
 
                         case 3:
-                            this.bodyfat = -517.634542700836 + -0.182789105182742 * this.age + -1.89867674831734 * this.weight + 4.52050229049177 * this.height + -12.3264718306416 * this.adiposity + 5.78399759359791 * this.neck + 18.0734734906965 * this.chest + -1.12236748532221 * this.abdomen + 16.0670417539698 * this.hip + 5.28301314944288 * this.thigh + -20.770413943282 * this.knee + -95.5728914279405 * this.ankle + 4.76001194682994 * this.biceps + -1.55903088381135 * this.forearm + -1.5233748797113 * this.wrist + -0.0441040957475888 * this.age * this.ankle + 0.0681618331256203 * this.age * this.wrist + 0.0449975566868278 * this.weight * this.height + 0.0960934062846427 * this.weight * this.chest + -0.633201002923837 * this.weight * this.ankle + -0.109550739034761 * this.height * this.chest + -0.0720994464973145 * this.height * this.hip + 0.337440472213329 * this.height * this.ankle + -0.0805027185270307 * this.height * this.biceps + 0.128980835824561 * this.height * this.forearm + 0.348629072988867 * this.adiposity * this.neck + -0.228298697411573 * this.adiposity * this.chest + -0.605071571614284 * this.adiposity * this.knee + 1.68049590579812 * this.adiposity * this.ankle + -0.320250852913665 * this.adiposity * this.biceps + 0.464170698711911 * this.adiposity * this.forearm + -0.150903909082736 * this.neck * this.hip + -0.0384326905147307 * this.chest * this.abdomen + 0.168641088530069 * this.chest * this.knee + -0.125522699914563 * this.chest * this.forearm + 0.0438592132913449 * this.abdomen * this.thigh + 0.17400205801669 * this.abdomen * this.wrist + -0.0658919993308778 * this.hip * this.thigh + 0.0971626081740445 * this.hip * this.knee + 0.227137751503346 * this.hip * this.biceps + -0.261930887996169 * this.hip * this.wrist + -0.0801662650430524 * this.thigh * this.biceps + 0.399563854618406 * this.knee * this.ankle + 1.68222261962102 * this.ankle * this.wrist + -1.11281895525546 * this.forearm * this.wrist
+                            this.bodyfat = -48.0072504350801 + 1.27253573079652 * this.hip + -0.00297215797092711 * this.height * this.hip + 0.0216282944397805 * this.abdomen * this.neck + -0.0239823087192233 * this.hip * this.neck + 0.00654955084837439 * this.height * this.forearm + -0.0491607870492823 * this.forearm * this.wrist
                             break;
                     }
                 } else if (this.measure == "i") {
